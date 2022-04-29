937239527306636a80705704afc28ad5cf29209b
Vijay <vijayakumar.marada@otis.com>
2022-04-29 20:16:16 +0530

    Added home controller and swagger suppoort

---
+++ KubernetesPractice/NetCore5API/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Dynamic;
+
+namespace NetCore5API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public string GetSystemDetails()
+        {
+            dynamic response = new ExpandoObject();
+            response.HostName = Environment.MachineName;
+            response.LocalIPAddress = Request.HttpContext.Connection.LocalIpAddress.ToString();
+            response.LocalPort= Request.HttpContext.Connection.LocalPort.ToString();
+            response.RemoteIpAddress= Request.HttpContext.Connection.RemoteIpAddress.ToString();
+            response.RemotePort = Request.HttpContext.Connection.RemotePort.ToString();
+            response.RequestHeaders = Request.HttpContext.Request.Headers;
+            response.EnvVariables = Environment.GetEnvironmentVariables(EnvironmentVariableTarget.Process);
+            return JsonConvert.SerializeObject(response);
+        }
+    }
+}
