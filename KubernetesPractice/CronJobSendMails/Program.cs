3e7c34492242e8d2612e8879cf989be290a1bcd4
Vijay <vijayakumar.marada@otis.com>
2022-05-03 13:35:23 +0530

    Cron job and spec update

---
+++ KubernetesPractice/CronJobSendMails/Program.cs
@@ -0,0 +1,50 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using MimeKit.Text;
+using System;
+
+namespace CronJobSendMails
+{
+    internal class Program
+    {
+        public static string Secret = "9gyQgfrWDgrtCbRjIUTvdFEdgsSGTWUSrNWW1gWvwPQYHd";
+        public static int RefreshTokenTTL = 2;
+        public static string EmailFrom = "info@vijay.com";
+        public static string SmtpHost = "smtp-relay.sendinblue.com";
+        public static int SmtpPort = 587;
+        public static string SmtpUser = "vijaychamp@msn.com";
+        public static string SmtpPass = "k3IZDferDWXqE9P7xsw34F2Qv8A";
+
+        static void Main(string[] args)
+        {
+            SendEmail();
+        }
+
+        private static void SendEmail()
+        {
+            string RandomId= Guid.NewGuid().ToString();
+            Send(
+                to: SmtpUser,
+                subject: "Mail from Kubernetes Cluster",
+                html: $@"<h4>Hey..!! This Email is fom Kubernetes Cron Job: Cluster time: {DateTime.Now} ans Random Id: {RandomId} </h4>"
+            );
+        }
+        private static void Send(string to, string subject, string html, string from = null)
+        {
+            // create message
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(from ?? EmailFrom));
+            email.To.Add(MailboxAddress.Parse(to));
+            email.Subject = subject;
+            email.Body = new TextPart(TextFormat.Html) { Text = html };
+
+            // send email
+            using var smtp = new SmtpClient();
+            smtp.Connect(SmtpHost, SmtpPort, SecureSocketOptions.StartTls);
+            smtp.Authenticate(SmtpUser, SmtpPass);
+            smtp.Send(email);
+            smtp.Disconnect(true);
+        }
+    }
+}
---
+++ KubernetesPractice/CronJobSendMails/README.md
@@ -0,0 +1,55 @@
+﻿#### Deploy Cron jobs in kubernetes cluster.. 
+
+In this example, I have used mail server to send a mail for every 1 min as a Job
+
+Below is the yml file, bit straight forward. much easy to understand
+
+```
+apiVersion: batch/v1
+kind: CronJob
+metadata:
+  name: sendmail
+spec:
+  suspend: true  # flag used to suspend the job (true/false)
+  schedule: "* * * * *"  //Runs every 1 min
+  jobTemplate:
+    spec:
+      template:
+        spec:
+          containers:
+          - name: sendmail
+            image: vijaychamp/cronsendmail:0.0.3 //a .net core console application image with .net core runtime
+            imagePullPolicy: IfNotPresent
+            #command:
+            #- /bin/sh
+            #- -c
+            #- date; echo Hello from the Kubernetes cluster
+          restartPolicy: OnFailure
+
+```
+
+See cron jon status and logs
+
+```
+kubectl get cronjob sendmail
+kubectl get jobs --watch
+
+pods=$(kubectl get pods --selector=job-name=sendmail-4111706356 --output=jsonpath={.items[*].metadata.name})
+kubectl logs $pods
+```
+
+Suspend corn job
+```
+kubectl patch cronjobs sendmail -p '{"spec" : {"suspend" : true }}'
+if above command not working, then change the yml file suspend to true and vice versa for running again
+spec:
+  suspend: true
+
+```
+
+If you dont want to run this job, use below command to delete it.
+
+```
+kubectl delete cronjob sendmail
+```
+
